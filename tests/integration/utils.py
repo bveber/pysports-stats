49c5c7a437073b6595224e9788d65e8f7319d6a9
bveber <veber.brandon@gmail.com>
2022-11-22 11:48:08 -0600

    fixed ncaab teams

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -4,4 +4,5 @@ from pyquery import PyQuery as pq
 
 def read_file(filename, sport, stat_type):
     filepath = os.path.join(os.path.dirname(__file__), stat_type, sport, filename)
-    return pq(open('%s' % filepath, 'r', encoding='utf8').read())
+    file_contents = open('%s' % filepath, 'r', encoding='utf8').read()
+    return pq(file_contents)
